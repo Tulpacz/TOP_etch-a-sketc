2c7c66bc4bc2f4c5ef7b759ca1eedec98cada295
Tulpacz <dawidlaska17@gmail.com>
2023-11-18 14:01:17 +0100

    donee

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-function populateBoard(size){let board = document.querySelector(".board");
-board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+let color = "black"
+let click = true
+
+function populateBoard(size){
+let board = document.querySelector(".board");
 let squares = board.querySelectorAll("div");
 squares.forEach((div) => div.remove());
-
+board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
 for(let i=0;i<size*size;i++){
     let square = document.createElement("div");
+    square.addEventListener("mouseover", colorSquare);
     square.style.backgroundColor = "blue";
     board.insertAdjacentElement("beforeend", square);
 
@@ -15,5 +19,45 @@ for(let i=0;i<size*size;i++){
 populateBoard(16);
 
 function changeSize(input){
+    
+    if(input >=2 && input <=100){
+    document.querySelector(".error").style.display = "none";
     populateBoard(input);
+    }
+    else{
+        document.querySelector(".error").style.display = "flex";
+    }
 }
+
+function colorSquare(){
+    if(click){if(color == 'random'){
+        this.style.backgroundColor = `hsl(${Math.random()*360}, 100%, 50%)`;
+    }
+    else
+    {this.style.backgroundColor = color};}
+}
+
+function changeColor(choice){
+    color = choice;
+}
+
+function resetBoard(){
+    let board = document.querySelector(".board");
+    let squares = board.querySelectorAll("div");
+    squares.forEach((div) => div.style.backgroundColor = `blue`);
+}
+
+document.querySelector(`body`).addEventListener(`click`, (e)=>{
+    if(e.target.tagName != `BUTTON`){
+        click = !click;
+
+        if(click)
+        {
+          document.querySelector('.mode').textContent = "Mode: Coloring";
+        }
+        else{
+            document.querySelector('.mode').textContent = "Mode: Not coloring";
+        }
+    }
+
+    });
